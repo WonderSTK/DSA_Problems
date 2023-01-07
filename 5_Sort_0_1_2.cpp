54d1b068242336238ed6810740cdcce63f451fd3
Mehul Kumar <45559139+WonderSTK@users.noreply.github.com>
2023-01-07 17:18:05 +0530

    Create 5_Sort_0_1_2.cpp

---
+++ 5_Sort_0_1_2.cpp
@@ -0,0 +1,24 @@
+// (without using sort function )
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   //   Write your code here
+    int low= 0;
+    int high= n-1;
+    int mid=0;
+    while(mid<=high){ 
+        if(arr[mid]==0 && mid<=high){
+            swap(arr[mid],arr[low]);
+            mid++;
+            low++;
+        }      
+        if(arr[mid]==1 && mid<=high){
+            mid++;
+        }      
+        if(arr[mid]==2 && mid<=high){
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+}
