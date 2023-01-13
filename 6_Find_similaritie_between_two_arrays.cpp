5be4562ee0620c2faa4232d99d6b56308c28bfc6
Mehul Kumar <45559139+WonderSTK@users.noreply.github.com>
2023-01-13 16:57:39 +0530

    Create  6_Find_similaritie_between_two_arrays.cpp

---
+++ 6_Find_similaritie_between_two_arrays.cpp
@@ -0,0 +1,18 @@
+  sort(arr1.begin(),arr1.end());
+    sort(arr2.begin(),arr2.end());
+    int count = 0;
+    int i = 0;
+    int j = 0;
+    while(i<arr1.size() && j<arr2.size()){
+        if(arr1[i]<arr2[j]) i++;
+        else if(arr2[j]<arr1[i]) j++;
+        else if(arr1[i++]==arr2[j++]) count++;
+    }
+    
+    unordered_set<int>s1;
+    for(int i = 0; i<max(n,m); i++){
+        if(i<n) s1.insert(arr1[i]);
+        if(i<m) s1.insert(arr2[i]);
+    }
+    
+    return {count,s1.size()};
